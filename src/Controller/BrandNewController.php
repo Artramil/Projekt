5879bcf9af367208e59dfb53d5aa55dcd5e5f068
Artem Kovalskiy <johndoe@exampomle.com>
2018-08-24 10:33:54 +0300

    controler & forms

---
+++ src/Controller/BrandNewController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BrandNewController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index()
+    {
+        return $this->render('brand_new/index.html.twig');
+    }
+}
